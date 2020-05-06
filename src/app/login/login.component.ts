d58804f4a97c46214a4ff08b43dac142a526123d
Marcos <marcos@localhost.localdomain>
2020-05-05 22:07:18 -0300

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoginService } from '../services/login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,20 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   public login;
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
     this.login = {};
   }
 
   loginF(){
-    console.log(this.login);
-    console.log(this.login.name);
-    console.log(this.login.senha);
+    // console.log(this.login);
+    // console.log(this.login.name);
+    // console.log(this.login.senha);
+    this.loginService.doLogin(this.login);
   }
 
   evento(event: any){
-    console.log(event.target.value);
+    // console.log(event.target.value);
   }
 
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private headers: HttpHeaders;
+  private local = "http://localhost:8080/";
+  private loginURL = 'oauth';
+  constructor(private http: HttpClient) { }
+  doLogin(login){
+    this.headers = this.getHeaders();
+    return this.post("oauth", login)
+      .toPromise()
+      .then(
+        response => {
+          console.log(response);
+          console.log('logou')
+          // this.router.navigate(["home"]);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+  get = (URL: string, params = null): Observable<Object> => {
+    return this.http.get(this.api + URL, {
+      headers: this.getHeaders(),
+      params
+    });
+  };
+
+  post = (URL: string, data: any): Observable<Object> => {
+    return this.http.post(this.local + this.loginURL, data, { headers: this.getHeaders() });
+  };
+
+  delete = (URL: string): Observable<Object> => {
+    return this.http.delete(this.local + this.loginURL, { headers: this.getHeaders() });
+  };
+
+  patch = (URL: string, data: any): Observable<Object> => {
+    return this.http.patch(this.local + this.loginURL, data, {
+      headers: this.getHeaders()
+    });
+  };
+
+  put = (URL: string, data: any): Observable<Object> => {
+    return this.http.put(this.local + this.loginURL, data, { headers: this.getHeaders() });
+  };
+  getHeaders() {
+    return (this.headers = new HttpHeaders()
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      // .append("Authorization", `Bearer ${this.getToken()}`)
+      );
+  }
+}
